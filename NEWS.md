d196ae2eb9e9d6d86539f51a4125fa3a120b4b31
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-05-12 23:09:21 +0200

    Adding Google typology

--- NEWS.md
+++ NEWS.md
@@ -2,3 +2,9 @@
 
 * Added a `NEWS.md` file to track changes to the package.
 * Preparing a release candidate with Travis-CI integration.
+
+# regions 0.1.0
+* Submitted to CRAN
+
+# regions 0.1.1.0001
+* Adding the `google_nuts_matchtable` by Istvan Zsoldos for joining data from the Google Mobility Reports with Eurostat datasets.
---
+++ R/data-google_nuts_matchtable.R
@@ -0,0 +1,17 @@
+#' Google Mobility Report European Correspondence Table 
+#'
+#' A dataset containing the correspondence table between the EU 
+#' NUTS 2016 typology and the typology used by Google in the
+#' Google Mobility Reports.
+#'
+#' @format A data frame with 724 rows and 5 variables:
+#' \describe{
+#'   \item{country_code}{ISO 3166-1 alpha2 code}
+#'   \item{google_region_level}{Hierachical level in the Google Mobility Reports}
+#'   \item{code_1999}{The name used by Google.}
+#'   \item{code_2016}{NUTS code in the 2016 definition}
+#'   \item{typology}{country, NUTS1, NUTS2 or NUTS3}
+#' }
+#' @source \url{https://ec.europa.eu/eurostat/web/nuts/history/}
+#' @author István Zsoldos
+"google_nuts_matchtable"
